f920a2cd9277eed2377438cd202d5d85b9dc78ab
Tuomas Toivonen <toivotuo@kasvua.org>
2020-09-21 22:42:24 +0300

    Add basic Django admin for Settler app

--- tapestry/settler/admin.py
+++ tapestry/settler/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from settler.models import Transaction
+from settler.models import Account
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'currency',
+        'description',
+    )
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    def balance(self):
+        return "{:8.2f}".format(self.get_balance())
+
+    list_display = (
+        'pk',
+        'handle',
+        'currency',
+        balance,
+    )
+
+    list_filter = (
+        'currency',
+    )
