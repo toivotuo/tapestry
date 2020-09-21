6f49d26632ca6dee2b8d066ee1b6db1a62a2345c
Tuomas Toivonen <toivotuo@kasvua.org>
2020-09-21 18:44:20 +0300

    Add settler.Transaction.add_transactio() method

--- tapestry/settler/models.py
+++ tapestry/settler/models.py
@@ -3,16 +3,69 @@ from django.core import validators
 
 # FIXME: Add UUID for all the models for reference in RESTful APIs.
 
+# FIXME: This double-entry accounting module should be moved into a
+# separate app called 'pacioli' that can be used by Settler or
+# something else. Name coming from Luca Pacioli of Summa Arithmetica
+# fame.
+
+class TransactionManager(models.Manager):
+
+    def add_transaction(self, currency, transfers):
+        """Add a single Transaction where 'currency' is the Transaction
+        currency, and 'transfers' is a list of data to create Transfer
+        objects with keys 'account', 'amount' and 'description'. The
+        'account' is an Account.handle. The Account is created if it
+        doesn't exist. The amounts of all Transfers must add up to a
+        zero. Returns the created Transaction.
+
+        """
+        # FIXME: Run in a database transaction!
+
+        from decimal import Decimal
+        transfer_sum = Decimal()
+
+        # Create the Transaction to hold the Transfers
+        tx = Transaction.objects.create(
+            currency=currency,
+        )
+
+        # Create the Transfers
+        for t in transfers:
+            account = Account.objects.get_or_create(
+                handle=t['account'],
+                currency=tx.currency,
+            )[0]
+            amount = Decimal(t['amount'])
+            Transfer.objects.create(
+                transaction=tx,
+                account=account,
+                amount=amount,
+                description=t.get('description', ''),
+            )
+            transfer_sum += amount
+
+        if transfer_sum:
+            # FIXME: Is there a more Django native exception to raise?
+            raise ValueError("Transfers of a Transaction must sum to zero")
+
+        return tx
+
 class Transaction(models.Model):
     """A Transaction represents a single accounting journal entry that
     groups together one or more debits and credits on Accounts as
     represented by Transfers.
 
     """
+    # FIXME: We do not have validation on database level that the
+    # Transfers of a Transaction sum to zero.
+
     currency = models.CharField(max_length=3)
     description = models.CharField(max_length=140)
 
     objects = TransactionManager()
+
+    def __str__(self):
+        return "{} {}".format(self.pk, self.currency)
 
 class Transfer(models.Model):
     """A Transfer is a single debit or a credit on an Account. Multiple
