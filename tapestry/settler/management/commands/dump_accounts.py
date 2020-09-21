ee1a53004f253e0b199b3e28cae312fb02aa1612
Tuomas Toivonen <toivotuo@kasvua.org>
2020-09-21 18:46:04 +0300

    Add 'dump_accounts' helper management command
    Shows the balance sheet within the Settler app.

---
+++ tapestry/settler/management/commands/dump_accounts.py
@@ -0,0 +1,26 @@
+# Debugging management command to dump the balance of all accounts in
+# the accounting modules of the Settler.
+
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    help = "Dump the balance of all accounts "
+
+    def add_arguments(self, parser):
+        pass
+
+    def success(self, message):
+        self.stdout.write(self.style.SUCCESS(message))
+
+    def handle(self, *args, **options):
+        from decimal import Decimal
+        from settler.models import Account
+
+        accounts = Account.objects.filter().order_by('handle', 'pk')
+
+        for account in accounts:
+            print("{:>4} {:<16} {:>8.2f}".format(
+                account.pk,
+                account.handle or '-',
+                account.get_balance(),
+            ))
