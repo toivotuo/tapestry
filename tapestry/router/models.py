7bef170ab9aa9c51bd32c9ca1bab51814723a1b9
Tuomas Toivonen <toivotuo@kasvua.org>
2020-10-04 00:56:26 +0300

    Evolve router.models.SepaRoute to support SCL-Directory

--- tapestry/router/models.py
+++ tapestry/router/models.py
@@ -3,32 +3,38 @@ from django.db import models
 from django.conf import settings
 from localflavor.generic.models import BICField
 from django_countries.fields import CountryField
-
+from tapestry.constants import PaymentSchemeChoice
+from impsepa.constants import SepaCsmChoice
 
 class SepaRoute(models.Model):
     """Represent an available route to a SEPA destination."""
 
     # FIXME: Could terminology here be aligned with BGP4?
 
+    # FIXME: We must store here the source of where the data comes
+    # from. Which directory and which version of the directory.
+
     class ReachabilityChoice(models.TextChoices):
         DIRECT = 'direct', 'Direct Participant'
         INDIRECT = 'indirect', 'Indirect Participant'
         UNKNOWN = 'unknown', 'Unknown'
 
-
-    SCHEME_CHOICES = [(s, s) for s in settings.SUPPORTED_PAYMENT_SCHEMES]
-
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
-    scheme = models.CharField(max_length=70, blank=False, choices=SCHEME_CHOICES)
-    external_key = models.CharField(max_length=16, blank=False)
+    scheme = models.CharField(max_length=70, blank=False,
+                              choices=PaymentSchemeChoice.choices)
+    external_key = models.CharField(max_length=16, blank=True)
     bic = BICField(blank=False)
     psp_name = models.CharField(max_length=140, blank=False)
-    psp_city = models.CharField(max_length=35, blank=False)
-    psp_country = CountryField()
-    reachable_via = models.CharField(max_length=4, blank=True)
+    psp_city = models.CharField(max_length=35, blank=True)
+    psp_country = CountryField(blank=True)
+    reachable_via = models.CharField(max_length=4, blank=True,
+                                     choices=SepaCsmChoice.choices)
     reachability_type = models.CharField(max_length=16, blank=False,
                                           choices=ReachabilityChoice.choices)
     intermediary_bic = BICField(blank=True)
     preferred_route = models.BooleanField(default=False)
-    valid_from = models.DateField(null=True, blank=True)
-    valid_to = models.DateField(null=True, blank=True)
+    valid_from = models.DateTimeField(null=True, blank=True)
+    valid_to = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'SEPA route'
