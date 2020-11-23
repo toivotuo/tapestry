f4a56f68b33465cc510263900f57df384c5d8d8f
Tuomas Toivonen <toivotuo@kasvua.org>
2020-11-23 20:36:26 +0200

    Add helper script to create LaTeX out of Tapestry ReST docs

---
+++ docs/texify.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Convert selected files to LaTeX and copy for inclusion in the
+# appendices of the TLBOP LyX document.
+
+cat <<EOF | while read f
+introduction
+design
+implementation
+development
+ledger
+router
+clearer
+settler
+fex
+impsepa
+impsic
+imp8583
+EOF
+do
+    echo ${f}
+    pandoc -o ~/tlbop/repo/tapestry/${f}.tex ${f}.rst
+done
